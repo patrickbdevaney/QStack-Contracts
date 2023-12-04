62bcd076458fb3449c4b90285087be49a63b9ef7
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2023-12-04 02:59:51 -0500

    make a contribution, getProjectFunds, quadratic matching

--- contracts/proposal.ts
+++ contracts/proposal.ts
@@ -4,23 +4,45 @@ declare global {
 
 import { Contract } from '@algorandfoundation/tealscript';
 import { ProjectProposal } from './types/ProjectProposal';
+import { Contribution } from './types/Contribution';
 
 class proposal extends Contract {
   projectProposals: Map<number, ProjectProposal> = new Map();
+  projectContributions: Map<number, Contribution[]> = new Map();
+
+  
 
   async submitProjectProposal(projectProposal: ProjectProposal) {
     
   
-    // Create an instance of the proposal class
-    const proposalContract = new proposal();
+    // Track contributions for the project
+    const projectID = this.projectProposals.size + 1;
+    const projectContributions = this.projectContributions.get(projectID) || [];
+    const contribution = {
+      contributor: msg.sender,
+      amount: projectProposal.funding,
+    };
+    projectContributions.push(contribution);
+    this.projectContributions.set(projectID, projectContributions);
   
-    // Call the accountBalance method on the instance
+    // Calculate matched funds using the quadratic funding formula
+    const totalContributions = projectContributions.reduce((sum, contribution) => sum + contribution.amount, 0);
+    const matchedFunds = ((totalContributions + 1) ** (1/2) - 1) ** 2;
   
+    // Update project proposal with matched funds
+    projectProposal.matchedFunds = matchedFunds;
   
-    const projectProposalID = this.projectProposals.size + 1;
-    this.projectProposals.set(projectProposalID, projectProposal);
+    // Store the project proposal in the map
+    this.projectProposals.set(projectID, projectProposal);
   }
 
+  async getProposalFundingData(proposalID: number): Promise<ProjectProposal> {
+    const projectProposal = this.projectProposals.get(proposalID);
+    if (!projectProposal) {
+      throw new Error(`Proposal with ID ${proposalID} not found`);
+    }
+    return projectProposal;
+  }
 }
 
   
---
+++ contracts/types/Contribution.ts
@@ -0,0 +1,4 @@
+export interface Contribution {
+    contributor: Address; // Address of the contributor
+    amount: number; // Amount contributed
+  }
